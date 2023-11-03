fa631834aeed4d1892638eb6364a66fa967bc13f
A. Bórquez <andres.borquezc@gmail.com>
2023-11-03 21:13:02 +0100

    Updated AliDPG macro according to opt standard - also increased the number of injected anti-neutrons to 100

--- GeneratorCustom.C
+++ GeneratorCustom.C
@@ -1,11 +1,36 @@
-AliGenerator *GeneratorCustom(Char_t reaction_channel = 'A', Double_t mass_sexaquark = 1.8) {
+AliGenerator *GeneratorCustom(TString opt = "") {
+
+    /* Parse option string */
+
+    // `opt` should be formatted as `"<reaction_channel><mass_sexaquark>"`
+    // - where <reaction_channel> should be replaced by a single-digit char from 'A' to 'H'
+    //   - 'A' : AntiSexaquark + Neutron -> AntiLambda, K0
+    //   - 'B' : AntiSexaquark + Neutron -> AntiLambda, K0, Pi-, Pi+
+    //   - 'C' : AntiSexaquark + Neutron -> AntiProton, K0, K0, Pi+
+    //   - 'D' : AntiSexaquark + Proton  -> AntiLambda, K+
+    //   - 'E' : AntiSexaquark + Proton  -> AntiLambda, K+, Pi-, Pi+
+    //   - 'F' : AntiSexaquark + Proton  -> AntiProton, K+, K0, Pi+
+    //   - 'G' : AntiSexaquark + Neutron -> Xi+, Pi-
+    //   - 'H' : AntiSexaquark + Proton  -> AntiProton, K+, K+, Pi0
+    // - and <mass_sexaquark> corresponds to the mass of the injected anti-sexaquark
+    //   - we're interested in: 1.73, 1.8, 1.87, 1.94, 2.01
+    // examples of valid `opt`: "A1.8", "B1.73", "C2.01", etc.
+
+    Char_t reaction_channel = 'A';
+    Double_t mass_sexaquark = 1.8; // GeV/c^2
+    if (opt != "") {
+        reaction_channel = opt(0);
+        mass_sexaquark = ((TString)opt(1, opt.Length())).Atof();
+    }
+
+    /* Initialize generators */
 
     AliGenCocktail *ctl = (AliGenCocktail *)GeneratorCocktail("Hijing+AntiNeutron+AntiSexaquark");
 
     AliGenHijing *hij = (AliGenHijing *)GeneratorHijing();
     ctl->AddGenerator(hij, "Hijing", 1.);
 
-    AliGenerator *ani = GeneratorInjector(40, -2112, 0., 6., -0.8, 0.8);
+    AliGenerator *ani = GeneratorInjector(100, -2112, 0., 6., -0.8, 0.8);
     ctl->AddGenerator(ani, "Injector (Anti-Neutron)", 1.);
 
     AliGenSexaquarkReaction *sr = new AliGenSexaquarkReaction(20, mass_sexaquark, reaction_channel);
